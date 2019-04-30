42aee8d7e8ed3dedcbb9dced0d21c5fc89c6efd4
Nkosikhona Bulana <bulana@Nkosikhonas-Mac-mini.local>
2019-04-30 22:09:31 +0200

    hello xcode app done

---
+++ hello-xcode/hello-xcode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hello-xcode
+//
+//  Created by Nkosikhona Bulana on 2019/04/30.
+//  Copyright © 2019 Nkosikhona Bulana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
